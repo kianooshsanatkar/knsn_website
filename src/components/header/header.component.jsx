e4f75891b231156728dfef2e93f902b23362f135
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-12-21 15:44:10 +0330

    init header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export const Header = (params) => {
+
+    return <div className='header'>
+        <ul>
+            <li><a href="#section_me">Me</a></li>
+            <li><a href="#section_about">About</a></li>
+            <li><a href="#section_services">Services</a></li>
+            <li><a href="#section_experiences">Experiences</a></li>
+            <li><a href="#section_article">Articles</a></li>
+            <li><a href="#section_framework">Frameworks</a></li>
+            <li><a href="#section_contact">Contact</a></li>
+        </ul>
+    </div>
+    
+}
